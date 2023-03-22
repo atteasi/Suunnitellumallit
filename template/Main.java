9d295cc3f7e0d94bcac273d729b4a60317f7a8fa
atteasi <atte.asikainen@metropolia.fi>
2023-03-22 10:45:22 +0200

    Template tehty

---
+++ template/Main.java
@@ -0,0 +1,8 @@
+package template;
+
+public class Main {
+    public static void main(String args[]){
+        NumeroPeli np = new NumeroPeli();
+        np.playOneGame(4);
+    }
+}
---
+++ template/NumeroPeli.java
@@ -0,0 +1,55 @@
+package template;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class NumeroPeli extends Game {
+
+    HashMap pisteet = new HashMap<Integer, Integer>();
+    boolean loppu = false;
+    @Override
+    void initializeGame() {
+        
+        System.out.println("Tervetuloa pelamaan satunnaislukupeliä!");
+    }
+
+    @Override
+    void makePlay(int player) {
+        Random rand = new Random();
+        int arvottuLuku = (rand.nextInt(10-1+1) + 1);
+
+        System.out.println("Pelaajalle " + player + " arvottiin luku " + arvottuLuku);
+        if(pisteet.get(player) == null){
+            pisteet.put(player, arvottuLuku);
+        } else {
+            int pisteMaara = arvottuLuku + (int) pisteet.get(player);
+            pisteet.put(player, pisteMaara);
+        }
+        int pisteita = (int) pisteet.get(player);
+        if(pisteita > 50){
+            loppu = true;
+        }
+    }
+
+    @Override
+    boolean endOfGame() {
+        if(loppu)
+            return true;
+        return false;
+    }
+
+    @Override
+    void printWinner() {
+        int voittaja = 0;
+        for(int i = 0; i < pisteet.size(); i++){
+            if(i != 0){
+                if((int) pisteet.get(i) > (int) pisteet.get(i - 1)){
+                    voittaja = i;
+                }
+            } else if (i == 0){
+            }
+        }
+        System.out.println("Pelin voitti pelaaja " + voittaja);
+    }
+    
+}
