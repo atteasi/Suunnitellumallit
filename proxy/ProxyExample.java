b2d57833c8483201116bf1881c7273701fe86428
atteasi <atte.asikainen@metropolia.fi>
2023-03-27 14:26:21 +0300

    proxy tehty

---
+++ proxy/ProxyExample.java
@@ -0,0 +1,42 @@
+package proxy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class ProxyExample {
+    /**
+     * Test method
+     */
+    public static void main(final String[] arguments) {
+
+        Scanner sc = new Scanner(System.in);
+        ProxyImage[] kuvat = {
+            new ProxyImage("kuva1"),
+            new ProxyImage("kuva2"),
+            new ProxyImage("kuva3"),
+            new ProxyImage("kuva4"),
+            new ProxyImage("kuva5"),
+        };
+
+        for(ProxyImage kuva: kuvat){
+            kuva.showData();
+        }
+
+        boolean jatkuu = true;
+
+        while(jatkuu){
+            System.out.println("Mitä kuvaa haluaisit katsoa? (lopeta syötteellä 0)");
+            int valinta = sc.nextInt();
+            if(valinta > kuvat.length || valinta < 0){
+                System.out.println("Virheellinen valinta!");
+            } 
+            else if(valinta == 0){
+                System.out.println("Poistutaan...");
+                jatkuu = false;
+            } else{
+                kuvat[valinta - 1].displayImage();
+            }
+        }
+        
+    }
+ }
